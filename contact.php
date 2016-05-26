6c82f74f503c6feb23bd471d07867a1e472c2944
ryanpousson <ryan.pousson@gmail.com>
2016-05-26 20:02:13 +1000

    Linked MongoDB database to website.
    Our database can be found here: https://mlab.com/login/
    
    Login details:
    
    username: throwdownattire
    password: dashcamera123
    
    you shouldn't need to login though. Please be careful if you do because I have all the data for another project I'm working on stored in there as well, it's in the "Shirts" collection. The collection I've made for us to work in is called "DashCamera". Basically, all the code in contact.php does is connect to our database, navigate to the DashCamera collection and increment a test document (MongoDB uses documents instead of tables) by 1.
    
    There are a couple of things you guys will need to do to get this working on your localhost (XAMPP, whatever it's called):
    1. Download the composer package manager and install it as per the instructions here: https://getcomposer.org/doc/00-intro.md
    2. Install the mongodb extension driver. I have windows so it's just a case of downloading this dll: https://pecl.php.net/package/mongo/1.6.14/windows and putting it in the php/ext folder of XAMPP. For mac either follow instructions here: http://php.net/manual/en/mongo.installation.php or here: https://github.com/mongodb/mongo-php-library (I think??).
    3. Open up a console window from XAMPP and run the composer commands to get the mongodb package to download as per the instructions here:
    https://getcomposer.org/doc/01-basic-usage.md
    
    Let me know if you have any problems, I've got it working locally for me, now I'm going to try get it working on Azure.

--- contact.php
+++ contact.php
@@ -1,7 +1,36 @@
 <html>
     <body>
     
-        <?php echo 'Welcome '. $_POST['name']. ' with email: '. $_POST['email']; ?>
+        <?php 
+            require __DIR__ . '/vendor/autoload.php';
+        
+            echo 'Welcome '. $_POST['name']. ' with email: '. $_POST['email'];
+            
+            /*
+            * Standard single-node URI format: 
+            * mongodb://[username:password@]host:port/[database]
+            */
+            $uri =  "mongodb://dashcamuser:dashcam123@ds063124.mlab.com:63124/throwdownattire";
+            $client = new MongoClient($uri);
+            $db = $client->selectDB("throwdownattire");
+            
+            /*
+            * Navigate to the DashCamera collection.
+            */
+            $dashcam = $db->DashCamera;
+            
+            // Find the test document I added.
+            $test = $dashcam->findOne(array('_id' => new MongoId('5746c01c51de0e9c1500002a')));
+            
+            // Incremeent its value by one and return the new value.
+            $newTest = $dashcam->findAndModify(
+                array('_id' => new MongoId('5746c01c51de0e9c1500002a')), // Find query.
+                array("test" => $test["test"] += 1), // Set new value.
+                null, // Some other parameter we don't need to worry about.
+                array("new" => true) // Return the updated value.
+            );
+            echo $newTest["test"]; // Print the new value.
+        ?>
     
     </body>
 </html>
