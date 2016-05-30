a74ef02c113dcf3c262c94fb1bfa77f413b35d21
ryanpousson <ryan.pousson@gmail.com>
2016-05-30 13:27:09 +1000

    Azure SQL attempt.

--- displayAnalytics.php
+++ displayAnalytics.php
@@ -1,13 +1,25 @@
 <?php
 	
 
-	$mysqli = new mysqli("localhost","root","","dashcam");
+	//$mysqli = new mysqli("localhost","root","","dashcam");
 
-    if ($mysqli->connect_error) {
-        die('Could not connect: ' . $conn->connect_error);
-    }
+    //if ($mysqli->connect_error) {
+      //  die('Could not connect: ' . $conn->connect_error);
+    //}
 
-
+	try  
+        {  
+            $serverName = "tcp:fsjk8c2g5g.database.windows.net,1433";  
+            $connectionOptions = array("Database"=>"dashcamera",  
+                "Uid"=>"dashuser", "PWD"=>"Dashpass1");  
+            $conn = sqlsrv_connect($serverName, $connectionOptions);  
+            if($conn == false)  
+                die(FormatErrors(sqlsrv_errors()));  
+        }  
+        catch(Exception $e)  
+        {  
+            echo("Error!");  
+        }
 
    
 	if ($result) {
