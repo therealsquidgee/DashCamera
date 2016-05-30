16152d5ae148699c8af216629559bb873266b51c
ryanpousson <ryan.pousson@gmail.com>
2016-05-31 02:11:39 +1000

    I need sleep but I tried to get the scroll_percentage sending to the db :(

--- analytics.php
+++ analytics.php
@@ -29,15 +29,7 @@ var_dump($_POST);
             echo("Error!");  
         }
     
-    $query = "CREATE TABLE userdb(userid int IDENTITY(1,1) PRIMARY KEY,
-    ip_address varchar(25) NOT NULL,
-    auto_record bit NOT NULL,
-    auto_detect bit NOT NULL,
-    auto_forget bit NOT NULL,
-    email_address bit NOT NULL,
-    timer_count bit NOT NULL,
-    scroll_percentage float NOT NULL,
-    video_count bit NOT NULL)";
+    $query = "INSERT INTO userdb (ip_address,auto_record,auto_detect,auto_forget,email_address,timer_count,scroll_percentage,video_count)values (?,?,?,?,?,?,?,?)";
     $result = sqlsrv_query($conn, $query);
     if( $result === false ) {
         die(var_dump(sqlsrv_errors()));
@@ -46,14 +38,11 @@ var_dump($_POST);
     
     echo $query;
 
-    $stmt = $conn->prepare("INSERT INTO user (ip_address,auto_record,auto_detect,auto_forget,email_address,timer_count,scroll_percentage,video_count)values (?,?,?,?,?,?,?,?)");
-    echo $stmt;
-
-    $stmt->bind_param("siiisidi", $ip_address,$auto_record,$auto_detect,$auto_forget,$email_address,$timer_count,$scroll_percentage,$video_count);
+    //$stmt->bind_param("siiisidi", $ip_address,$auto_record,$auto_detect,$auto_forget,$email_address,$timer_count,$scroll_percentage,$video_count);
     
 
     //executes some statement somewhere
-    $stmt->execute();
+    //$stmt->execute();
 
     
 
@@ -62,8 +51,8 @@ var_dump($_POST);
     
 
 
-    $stmt->close();
-    $conn->close();
+    //$stmt->close();
+    sqlsrv_close($conn);
     echo "finished";
 ?>
 
